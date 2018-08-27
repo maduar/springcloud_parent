7d1ea87801609897d64668102f24707b0c2ceece
maduar <maduar86@gmail.com>
2018-08-28 00:03:40 +0800

    init

---
+++ springcloud_client/src/main/java/com/maduar/client/config/SecurityPermitAllConfig.java
@@ -0,0 +1,21 @@
+package com.maduar.client.config;
+
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/*
+ *
+ * @author maduar
+ * @date 27/08/2018 10:52 PM
+ * @email maduar@163.com
+ *
+ * */
+//@Configuration
+//public class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests().anyRequest().permitAll()
+//                .and().csrf().disable();
+//    }
+//}
