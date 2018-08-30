076da54ed72742aae6b1f6422c556849aef0ca0b
maduar <maduar86@gmail.com>
2018-08-30 10:24:40 +0800

    1. eureka 增加 security

--- eureka_server/src/main/java/com/maduar/eureka/server/EurekaServerApp.java
+++ eureka_server/src/main/java/com/maduar/eureka/server/EurekaServerApp.java
@@ -1,16 +1,18 @@
 package com.maduar.eureka.server;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 /**
  * Hello world!
  */
+@EnableWebSecurity
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApp {
     public static void main(String[] args) {
-        SpringApplication.run(EurekaServerApp.class, args);
+        new SpringApplicationBuilder(EurekaServerApp.class).web(true).run(args);
     }
 }
