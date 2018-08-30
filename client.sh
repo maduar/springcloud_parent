d6df7beebd437b4576114f5ab5beabda1355ce07
maduar <maduar86@gmail.com>
2018-08-30 10:26:35 +0800

    1. parent update yml

--- client.sh
+++ client.sh
@@ -1,5 +1,4 @@
 source ~/.bash_profile 
 cd /Users/maduar/demo/java/springcloud_parent/springcloud_client
 mvn clean install -Dmaven.test.skip=true
-java -jar /Users/maduar/demo/java/springcloud_parent/springcloud_client/target/
-
+java -jar /Users/maduar/demo/java/springcloud_parent/springcloud_client/target/springcloud_client-1.0-SNAPSHOT.jar
