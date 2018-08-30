a4d6980b5e947571124212a8c98806c608480680
maduar <maduar86@gmail.com>
2018-08-30 10:26:06 +0800

    1. admin update yml

--- springcloud_client/src/main/java/com/maduar/client/ClientApp.java
+++ springcloud_client/src/main/java/com/maduar/client/ClientApp.java
@@ -1,16 +1,25 @@
 package com.maduar.client;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Hello world!
  */
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 @EnableEurekaClient
-@SpringBootApplication
 public class ClientApp {
+
     public static void main(String[] args) {
-        SpringApplication.run(ClientApp.class, args);
+        new SpringApplicationBuilder(ClientApp.class).web(true).run(args);
     }
 }
