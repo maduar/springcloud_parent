404a20ec6d6feb60a9e5777526eb01360e75e17d
maduar <maduar86@gmail.com>
2018-10-22 17:13:02 +0800

    add aspectj

--- zipkin/src/main/java/com/maduar/zipkin/ZipkinApp.java
+++ zipkin/src/main/java/com/maduar/zipkin/ZipkinApp.java
@@ -2,13 +2,14 @@ package com.maduar.zipkin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
 import zipkin.server.EnableZipkinServer;
 
 /**
  * Hello world!
  */
 @SpringBootApplication
-@EnableZipkinServer
+@EnableZipkinStreamServer
 public class ZipkinApp {
     public static void main(String[] args) {
         SpringApplication.run(ZipkinApp.class, args);
