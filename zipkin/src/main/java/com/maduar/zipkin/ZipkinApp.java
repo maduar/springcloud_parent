78daf7d3752ca00550fe28652318c14cb49d2c9b
maduar <maduar86@gmail.com>
2018-10-23 14:14:29 +0800

    update zipkin

--- zipkin/src/main/java/com/maduar/zipkin/ZipkinApp.java
+++ zipkin/src/main/java/com/maduar/zipkin/ZipkinApp.java
@@ -3,7 +3,11 @@ package com.maduar.zipkin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
-import zipkin.server.EnableZipkinServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import zipkin.storage.mysql.MySQLStorage;
+
+import javax.sql.DataSource;
 
 /**
  * Hello world!
@@ -13,5 +17,11 @@ import zipkin.server.EnableZipkinServer;
 public class ZipkinApp {
     public static void main(String[] args) {
         SpringApplication.run(ZipkinApp.class, args);
+    }
+
+    @Bean
+    @Primary
+    public MySQLStorage mySQLStorage(DataSource datasource) {
+        return MySQLStorage.builder().datasource(datasource).executor(Runnable::run).build();
     }
 }
